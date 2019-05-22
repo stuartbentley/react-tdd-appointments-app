53fc3f11500736d09784b5a9ac794517d7df5aac
Stuart Bentley <gstuartbentmely@gmail.com>
2019-05-22 12:14:55 +0100

    second test

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,15 +1,23 @@
 import React from 'react';
-   import ReactDOM from 'react-dom';
-import { Appointment } from '../src/Appointment';
+import ReactDOM from 'react-dom';
+import {Appointment} from '../src/Appointment';
+
 describe('Appointment', () => {
-	it('renders the customer first name', () => {
-		const customer = { firstName: 'Ashley' };
-const component = <Appointment customer={customer} />;
-const container = document.createElement('div');
-     document.body.appendChild(container);
-     ReactDOM.render(component, container);		
 
+    it('renders the customer first name', () => {
+        const customer = {firstName: 'Ashley'};
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+        ReactDOM.render(<Appointment customer={customer} />, container);
 
-expect(document.body.textContent).toMatch('Ashley');
+        expect(container.textContent).toMatch('Ashley');
+    });
+
+    it('renders another customer first name', () => {
+        const customer = {firstName: 'Jordan'};
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+        ReactDOM.render(<Appointment customer={customer} />, container);
+        expect(container.textContent).toMatch('Jordan');
+    });
 });
-   });
